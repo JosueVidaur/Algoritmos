24252e8bce773c8d445bd3ef0048f43d53f81c7a
Josue Vidaur <jovidaur@gmail.com>
2016-06-04 18:51:08 -0600

    agregada la clase nodo

---
+++ Nodo.java
@@ -0,0 +1,45 @@
+public class Nodo{
+  private String elemento;
+  public Nodo sig;
+  Nodo(String elemento){
+    this.elemento=elemento;
+    sig=null;
+  }
+
+  public String getElemento(){
+    return elemento;
+  }
+
+}
+
+public class Cola{
+  private Nodo primero;
+  private Nodo ultimo;
+    public Cola(){
+      primero=ultimo=null;
+    }
+
+    public bool vacio(){
+      return (primero==ultimo);
+    }
+
+    public String insertar(String elemento){
+      Nodo temporal=new Nodo(elemento);
+      if(vacio()==true){
+        primero=ultimo=temporal;
+      }
+      else{
+        ultimo.sig=temporal;
+      }
+    }
+
+    public String eliminar(String elemento){
+      private Nodo borrador;
+      if(vacio()==true){
+        System.out.println("La cola esta vacia, no es posible borrar elementos");
+      }
+      else{
+        primero=primero.sig;
+      }
+    }
+}
