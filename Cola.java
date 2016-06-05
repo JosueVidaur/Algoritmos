3a69c1377900609f5d3c33a6f5e781cf409e4a1a
Josue Vidaur <jovidaur@gmail.com>
2016-06-04 18:56:44 -0600

    Clase Cola agregada

---
+++ Cola.java
@@ -0,0 +1,32 @@
+
+public class Cola{
+  private Nodo primero;
+  private Nodo ultimo;
+    public Cola(){
+      primero=ultimo=null;
+    }
+
+    public bool vacio(){
+      return (primero==ultimo);
+    }
+
+    public String insertar(String elemento){
+      Nodo temporal=new Nodo(elemento);
+      if(vacio()==true){
+        primero=ultimo=temporal;
+      }
+      else{
+        ultimo.sig=temporal;
+      }
+    }
+
+    public String eliminar(String elemento){
+      private Nodo borrador;
+      if(vacio()==true){
+        System.out.println("La cola esta vacia, no es posible borrar elementos");
+      }
+      else{
+        primero=primero.sig;
+      }
+    }
+}
