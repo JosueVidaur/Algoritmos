5140200e47f4a5b1d703277061ee0409775bfa5b
RonyVidaur <rony.vidaur@gmail.com>
2016-06-04 19:24:03 -0600

    limpieza de codigo

--- Cola.java
+++ Cola.java
@@ -2,31 +2,32 @@
 public class Cola{
   private Nodo primero;
   private Nodo ultimo;
-    public Cola(){
-      primero=ultimo=null;
+
+  public Cola(){
+    primero=ultimo=null;
     }
 
-    public bool vacio(){
-      return (primero==ultimo);
+  public bool vacio(){
+    return (primero==ultimo);
     }
 
-    public String insertar(String elemento){
-      Nodo temporal=new Nodo(elemento);
-      if(vacio()==true){
-        primero=ultimo=temporal;
+  public String insertar(String elemento){
+    Nodo temporal=new Nodo(elemento);
+    if(vacio()==true){
+      primero=ultimo=temporal;
       }
-      else{
-        ultimo.sig=temporal;
+    else{
+      ultimo.sig=temporal;
       }
     }
 
-    public String eliminar(String elemento){
-      private Nodo borrador;
-      if(vacio()==true){
-        System.out.println("La cola esta vacia, no es posible borrar elementos");
+   public String eliminar(String elemento){
+    private Nodo borrador;
+    if(vacio()==true){
+      System.out.println("La cola esta vacia, no es posible borrar elementos");
       }
-      else{
-        primero=primero.sig;
+    else{
+      primero=primero.sig;
       }
     }
 }
